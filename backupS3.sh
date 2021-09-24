477a369d35fa889b62e1f1cecdbc0bc9d2bae513
Ace112131 <87170984+Ace112131@users.noreply.github.com>
2021-09-24 11:46:29 +0530

    Delete backupS3.sh

--- backupS3.sh
+++
@@ -1,9 +0,0 @@
-# (1) set up all the mysqldump variables
-DATE=`date +"%d_%b_%Y_%H%M"`
-SQLFILE=/var/backups/db/db_backup_${DATE}.sql
-DATABASE="demosite"
-USER="root"
-PASSWORD="ashu@123"
-sudo mysqldump -u ${USER} -p${PASSWORD} ${DATABASE}|gzip > ${SQLFILE}.gz
-sudo s3cmd -c /root/.s3cfg put ${SQLFILE}.gz s3://demo-buck112
-sudo find /var/backups/db/. -mtime +7 -exec rm {} \;
